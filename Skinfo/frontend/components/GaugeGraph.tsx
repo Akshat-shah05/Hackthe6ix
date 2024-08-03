9520085907d86825cba9cf638102550b8a0c7ea6
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-08-03 18:47:47 -0400

    customize gaugegraph

--- Skinfo/frontend/components/GaugeGraph.tsx
+++ Skinfo/frontend/components/GaugeGraph.tsx
@@ -5,7 +5,25 @@ import dynamic from 'next/dynamic';
 const GaugeComponent = dynamic(() => import('react-gauge-component'), { ssr: false });
 
 const GaugeGraph: React.FC = () => {
-  return <GaugeComponent />;
+  return <GaugeComponent
+  type = "semicircle"
+  arc={{
+    colorArray: ['blue', '#FF2121'],
+    padding: 0.02,
+    subArcs:
+      [
+        { limit: 40 },
+        { limit: 60 },
+        { limit: 70 },
+        {},
+        {},
+        {},
+        {}
+      ]
+  }}
+  pointer={{type: "blob", animationDelay: 0}}
+  value={50}
+/>;
 };
 
 export default GaugeGraph;
