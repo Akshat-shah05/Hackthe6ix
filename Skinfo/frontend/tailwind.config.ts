4ba37c252b09a2ce0e21b68b52e643cb25ef8279
akshat-shah05 <akshatshah1605@gmail.com>
2024-08-03 21:43:00 -0400

    package

--- Skinfo/frontend/tailwind.config.ts
+++ Skinfo/frontend/tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         'custom-gradient': 'linear-gradient(180deg, #31c5f5, #f720e8)',
+        'custom-gradient-2': 'linear-gradient(180deg, #0f2027, #203a43, #2c5364)',
       },
     },
   },
