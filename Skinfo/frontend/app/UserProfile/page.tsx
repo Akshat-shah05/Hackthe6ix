b3fced3fd2c8a7b4dafb7afab7515f36f8dae9c7
akshat-shah05 <akshatshah1605@gmail.com>
2024-08-04 01:00:23 -0400

    Radar chart

--- Skinfo/frontend/app/UserProfile/page.tsx
+++ Skinfo/frontend/app/UserProfile/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import RadarChart from '@/components/RadarChart';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 // src/components/GridComponent.tsx
-const GridComponent = (results: any) => {
-  console.log(results)
+const UserProfile = (results: any) => {
+  useEffect(() => {
+    console.log(results)
+  })
   return (
     <div className="w-screen bg-custom-gradient-2">
       <div className="w-3/5 h-screen grid grid-cols-2 grid-rows-2 gap-8 p-4 bg-custom-gradient-2 pt-16 ">
@@ -12,7 +15,7 @@ const GridComponent = (results: any) => {
           </div>
           <div className="border border-white bg-transparent flex flex-col items-center rounded rounded-lg" >
             <h1 className="text-3xl mt-4 mb-8"> Primary Skin Concerns </h1>
-            <div className="w-100 h-100"><RadarChart /></div>
+            <div className="w-100 h-100"><RadarChart results={results[3]}/></div>
           </div>
           <div className="border border-white bg-transparent flex flex-row justify-center rounded rounded-lg" >
             <h1 className="text-3xl mt-4"> Current Products Used </h1>
@@ -25,4 +28,4 @@ const GridComponent = (results: any) => {
   );
 };
 
-export default GridComponent;
+export default UserProfile;
