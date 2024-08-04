b7e838891548d7cfb8618316388718f376ff11c0
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-08-03 21:19:29 -0400

    Gauge graph layout

--- Skinfo/frontend/app/AnalysisPage/page.tsx
+++ Skinfo/frontend/app/AnalysisPage/page.tsx
@@ -1,9 +1,17 @@
 'use client'
 import React from 'react';
 import GaugeGraphcomp from '../../components/GaugeGraph';
+import NavBar from '../../components/NavBar';
 
 const AnalysisPage: React.FC = () => {
-  return <GaugeGraphcomp />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-1 flex items-center justify-center p-4">
+        <GaugeGraphcomp />
+      </main>
+    </div>
+  );
 };
 
 export default AnalysisPage;
