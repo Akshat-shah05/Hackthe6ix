e7fafac6b328c1fc0c6595fe10f3650f45daac96
Ally Guo <69687800+ally-223@users.noreply.github.com>
2024-08-04 06:18:05 -0400

    nav bar

--- Skinfo/frontend/components/NavBar.tsx
+++ Skinfo/frontend/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 // Define a type for your route items
@@ -9,25 +9,20 @@ interface Route {
 }
 
 const NavBar: React.FC = () => {
-  const [activeIndex, setActiveIndex] = useState<number>(0); // Manage the active icon state
-
   // Define your routes here
   const routes: Route[] = [
-    { id: 0, path: '/SkinQuiz', iconPath: "M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" },
+    { id: 0, path: '/', iconPath: "M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" },
     { id: 1, path: '/ScanningPage', iconPath: "M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" },
-    { id: 2, path: '/', iconPath: "M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" },
+    { id: 2, path: '/AnalysisPage', iconPath: "M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" },
   ];
 
   return (
     <nav className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full bg-white bg-opacity-70 shadow-md rounded-lg p-4">
       <ul className="flex justify-around">
-        {routes.map((route, index) => (
+        {routes.map((route) => (
           <li
             key={route.id}
-            className={`flex flex-col items-center cursor-pointer ${
-              activeIndex === index ? 'text-teal-700' : 'text-gray-500'
-            }`}
-            onClick={() => setActiveIndex(index)}
+            className="flex flex-col items-center cursor-pointer text-gray-500 hover:text-teal-700"
           >
             <Link href={route.path}>
               <svg
